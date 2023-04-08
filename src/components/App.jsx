adf4dbedbd41f4441b3f76a948e8345a37836a56
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-08 01:35:48 +0100

    Working on App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
-export default function App () {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "../Pages/Home"
+import Teams from "../Pages/Teams"
+import Players from "../Pages/Players"
+
+
+
+export default function App() {
   return (
     <div>
       Hash History Basketball League
