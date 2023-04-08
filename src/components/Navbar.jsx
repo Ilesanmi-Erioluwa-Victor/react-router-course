2897cbbd0e9fb21436f8e48a021deeb54cee6933
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-08 01:31:28 +0100

    Working on  Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+Link
+const Navbar = () => {
+  return (
+    <div className='container navbar'>
+      
+    </div>
+  )
+}
+Link
+export default Navbar
+
+
