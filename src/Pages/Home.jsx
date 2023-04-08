8769d6310c853636e195e183641a0e2c223975ac
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-08 02:30:38 +0100

    Stoped at video 7

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,23 +10,27 @@ const Home = () => {
   if (loading) {
     return null;
   }
-    return <div className="container">
-        <h1 className="large-header">
-            Hash History Basketball League
-        </h1>
-        
-        <h3 className="header text-center">
-            Select a team.
-        </h3>
+  return (
+    <div className="container">
+      <h1 className="large-header">Hash History Basketball League</h1>
 
-        <div className="home-grid">
-            {teamNames.map((id) => (
-                <Link key={id} to={`/${id}`}>
-                    <TeamLogo id={id} width="125px" />
-                </Link>
-            ))}
-        </div>
-  </div>;
+      <h3 className="header text-center">Select a team.</h3>
+
+      <div className="home-grid">
+        {teamNames.map((id) => (
+          <Link
+            key={id}
+            to={`/${id}`}
+          >
+            <TeamLogo
+              id={id}
+              width="125px"
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
