71e2e776da63d370c7326d471af3794b040ff46e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-08 02:12:47 +0100

    Working on Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import TeamLogo from "./TeamLogo";
+import useTeamNames from "../hooks/useTeamName";
+
 
 const Home = () => {
   return <div>Home</div>;
