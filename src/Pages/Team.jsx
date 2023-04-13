7eb93c02a7b5d087404ab42172a2d0eb6c5901a7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-13 04:29:38 +0100

    Working on Team Page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useTeamNames from "./hooks/useTeamNames";
+import useTeamArticles from "./hooks/useTeamArticles";
+
 
 const Team = () => {
     const { teamId } = useParams();
