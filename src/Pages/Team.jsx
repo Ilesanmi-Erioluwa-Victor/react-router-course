2037aad2a874a446a775c0568108a2b5f5363a6f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-13 04:37:51 +0100

    Working on Team Page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 import useTeamNames from "../hooks/useTeamNames";
 import useTeamArticles from "../hooks/useTeamsArticles";
-import useTeam from "../hooks/useTeam"
-import TeamLogo from "../components/TeamLogo"
-
+import useTeam from "../hooks/useTeam";
+import TeamLogo from "../components/TeamLogo";
 
 function useTeamPageData(teamId) {
-  const { } = useTeamNames();
+  const {} = useTeamNames();
 
-  const { } = useTeamArticles();
+  const {} = useTeamArticles(teamId);
+
+  const {} = useTeam(teamId);
 }
 
 const Team = () => {
-    const { teamId } = useParams();
-    return (
-    <div>
-      Team Page for { teamId}
-    </div>
-  )
-}
+  const { teamId } = useParams();
+  return <div>Team Page for {teamId}</div>;
+};
 
-export default Team
-
+export default Team;
