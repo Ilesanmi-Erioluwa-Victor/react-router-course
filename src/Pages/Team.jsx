88191fe50b53d26b12fed52a0f98bb996f1682b3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-13 04:50:07 +0100

    Working on Team Page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -8,16 +8,17 @@ import TeamLogo from "../components/TeamLogo";
 function useTeamPageData(teamId) {
   const { response: teamNames, loading: loadingTeamNames } = useTeamNames();
 
-  const { loading : loadingTeamArticles, response : articles} = useTeamArticles(teamId);
+  const { loading: loadingTeamArticles, response: articles } =
+    useTeamArticles(teamId);
 
   const { response: team, loadingTeam } = useTeam(teamId);
-  
+
   return {
     teamNames,
     team,
-    loading : loadingTeam || loadingTeamArticles || loadingTeamNames,
-    articles
-  }
+    loading: loadingTeam || loadingTeamArticles || loadingTeamNames,
+    articles,
+  };
 }
 
 const Team = () => {
@@ -25,14 +26,18 @@ const Team = () => {
 
   const { teamNames, team, loading, articles } = useTeamPageData(teamId);
 
+  console.log({ teamNames, team, articles });
+
   if (loading === true) {
-    return <p>Loading....</p>
+    return <p>Loading....</p>;
   }
 
   if (!teamNames.includes(teamId)) {
-    return <h1 className="text-center"> The {teamId} is not a valid Team...</h1>
-  } 
-  
+    return (
+      <h1 className="text-center"> The {teamId} is not a valid Team...</h1>
+    );
+  }
+
   return <div>Team Page for {teamId}</div>;
 };
 
