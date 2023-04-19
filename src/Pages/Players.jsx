8084ae0c17618be9be9a4f266eb54435f3bc77ee
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-19 03:35:30 +0100

    Working on Players Page

--- src/Pages/Players.jsx
+++ src/Pages/Players.jsx
@@ -4,6 +4,9 @@ import { useLocation, useSearchParams } from "react-router-dom";
 const Players = () => {
   const location = useLocation();
   const [searchParams] = useSearchParams();
+
+  const team = searchParams.get("teamId");
+  
   return <div>Players</div>;
 };
 
