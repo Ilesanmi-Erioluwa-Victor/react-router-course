2ea9a1530d0cd999e4d54bc058f3baf190189c53
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-19 04:05:38 +0100

    Working on Players Page

--- src/Pages/Players.jsx
+++ src/Pages/Players.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useLocation, useSearchParams } from "react-router-dom";
-import usePlayerNames from "../hooks/usePlayerNames"
+import usePlayerNames from "../hooks/usePlayerNames";
+
 const Players = () => {
   const location = useLocation();
   const [searchParams] = useSearchParams();
 
   const team = searchParams.get("teamId");
 
-
+  const {
+    response: names,
+    loading
+  } = usePlayerNames
 
   return <div className="container">Players for team {team}</div>;
 };
