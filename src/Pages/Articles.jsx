25c435f0e7d8aa4316d3808db51f77bacf3951ff
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-20 05:03:04 +0100

    Working on Articles Page

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Outlet, useParams } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
-import useTeamsArticles from "../hooks/useArticles"
+import useTeamsArticles from "../hooks/useArticles";
+
 const Articles = () => {
+  const { teamId } = useParams();
   return <div></div>;
 };
 
