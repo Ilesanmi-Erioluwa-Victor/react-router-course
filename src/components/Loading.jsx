018994ea81c2f1180370e5ba2fc265ae8ebffc1e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-20 18:40:58 +0100

    Creating a delayed loading component for Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,9 +3,22 @@ import React from "react";
 function Delayed({ children, wait = 500 }) {
   const [show, setShow] = React.useState(false);
 
+  React.useEffect(() => {
+    const timeOut = window.setTimeout(() => {
+      
+    }, wait)
+
+    return () =>
+  }, [])
+
   return show === true ? children : null
 }
 
 export const Loading = () => {
-  return <div className="loading center"></div>;
+  return (
+    <Delayed>
+      <div className="loading center"></div>;
+    </Delayed>
+  );
+ 
 };
