0380fa204acf3fc9ebe9774972efd9c0ff491070
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-20 17:10:02 +0100

    continued working

--- src/Pages/Articles.jsx
+++ src/Pages/Articles.jsx
@@ -6,12 +6,21 @@ import useTeamsArticles from "../hooks/useArticles";
 const Articles = () => {
   const { teamId } = useParams();
 
-    const { loading, response: articles } = useTeamsArticles(teamId);
-    
-    if (loading === true) {
-        return <p>LOADING...</p>
-    }
-  return <div></div>;
+  const { loading, response: articles } = useTeamsArticles(teamId);
+
+  if (loading === true) {
+    return <p>LOADING...</p>;
+  }
+  return (
+    <div className="container two-column">
+      <Sidebar
+        title={"Articles"}
+        list={articles.map((article) => article.title)}
+      />
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default Articles;
