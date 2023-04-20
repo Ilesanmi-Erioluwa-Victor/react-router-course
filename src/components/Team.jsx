c85c92888e8207f5f458e66160f4e6981b585186
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-20 01:55:47 +0100

    Working on teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -8,16 +8,38 @@ const Team = () => {
 
   const { loading, response: team } = useTeam(teamId);
 
-    if (loading === true) {
-        return null
-    }
-    return <div className="panel">
-        <div style={{ width: "100%" }}>
-            <TeamLogo id={team.id} className="center" />
-            <h1 className="medium-header">{team.name}</h1>
-            
+  if (loading === true) {
+    return null;
+  }
+  return (
+    <div className="panel">
+      <div style={{ width: "100%" }}>
+        <TeamLogo
+          id={team.id}
+          className="center"
+        />
+        <h1 className="medium-header">{team.name}</h1>
+        <ul className="info-list row">
+          <li>
+            Est. <div>{team.established.toUpperCase}</div>
+          </li>
+
+          <li>
+            Manager <div>{team.manager.toUpperCase}</div>
+          </li>
+          <li>
+            Coach <div>{team.coach.toUpperCase}</div>
+          </li>
+        </ul>
+        <Link
+          to={`/${teamId}`}
+          className="center btn-main"
+        >
+          {team.name} Team Page
+        </Link>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Team;
