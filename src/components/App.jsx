351db6e4c8f3c2e7f830e8ae61bed4ce3298c0ca
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-04-28 02:50:58 +0100

    working on protected route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -79,7 +79,7 @@ export default function App() {
       <div>
         <Suspense fallback={<Loading />}>
           <Navbar />
-            <Routers />
+          <Routers />
         </Suspense>
       </div>
     </Router>
